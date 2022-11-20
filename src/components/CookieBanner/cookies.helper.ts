17d51e1d5861233545fc4f34fc126afaec07d74d
GCHQ-Developer-847 <111882109+GCHQ-Developer-847@users.noreply.github.com>
2022-11-20 19:19:11 +0000

    feat(root): Add cookies banner
    Add cookies banner to store Google Analytics data.

---
+++ src/components/CookieBanner/cookies.helper.ts
@@ -0,0 +1,53 @@
+// One year
+const EXPIRY_IN_SECONDS = 31104000000;
+
+export const setCookie = (value: string) => {
+  const date = new Date();
+
+  date.setTime(date.getTime() + EXPIRY_IN_SECONDS);
+  document.cookie = `${value}; expires=${date.toUTCString()}; path=/;`;
+};
+
+export const consentCookieActioned = () =>
+  document.cookie.indexOf("cookie-consent") !== -1;
+
+export const consentCookieApproved = () =>
+  consentCookieActioned() &&
+  document.cookie.indexOf("cookie-consent=true") !== -1;
+
+export const setConsent = (consent: boolean) => {
+  // treat no consent as 'revoked' and delete existing cookies
+  if (!consent) {
+    /* eslint-disable */
+    document.cookie.replace(/(?<=^|;).+?(?=\=|;|$)/g, (name) =>
+      location.hostname
+        .split(".")
+        .reverse()
+        .reduce(
+          (domain) => (
+            (domain = domain.replace(/^\.?[^.]+/, "")),
+            (document.cookie = `${name}=;max-age=0;path=/;domain=${domain}`),
+            domain
+          ),
+          location.hostname
+        )
+    );
+    /* eslint-enable */
+  }
+
+  setCookie(`gatsby-plugin-google-analytics-gdpr_cookies-enabled=${consent}`);
+  setCookie(`cookie-consent=${consent}`);
+};
+
+export const isBotOrDoNotTrack = () => {
+  const isBot = /bot|crawler|spider|crawling/i.test(navigator.userAgent);
+
+  // Check if DoNotTrack is activated
+  const dnt = navigator.doNotTrack; //|| window.doNotTrack;
+  const isToTrack =
+    dnt !== null && dnt !== undefined
+      ? dnt && dnt !== "yes" && /*dnt !== 1 &&*/ dnt !== "1"
+      : true;
+
+  return isBot || !isToTrack;
+};
