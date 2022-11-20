e40e4544abc9384a8e02ad9bc9a88a36333bfcf4
GCHQ-Developer-847 <111882109+GCHQ-Developer-847@users.noreply.github.com>
2022-11-20 19:19:11 +0000

    feat(root): Add cookies banner
    Add cookies banner to store Google Analytics data.

---
+++ src/components/CookieBanner/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import {
+  IcSectionContainer,
+  IcTypography,
+  IcLink,
+  IcButton,
+} from "@ukic/react";
+
+import "./cookies.css";
+import {
+  consentCookieApproved,
+  consentCookieActioned,
+  isBotOrDoNotTrack,
+  setConsent,
+} from "./cookies.helper";
+
+const CookieBanner: React.FC = () => {
+  const [visible, setVisible] = useState(!consentCookieActioned());
+  const [submitted, setSubmitted] = useState(false);
+  const banner: React.RefObject<HTMLDivElement> = React.createRef();
+
+  const handleConsent = (consent: boolean) => {
+    setConsent(consent);
+    setSubmitted(true);
+  };
+
+  useEffect(() => {
+    // set focus to cookies banner when submitted, to focus on message
+    if (banner.current) {
+      banner.current.focus();
+    }
+  }, [submitted]);
+
+  if (!visible || isBotOrDoNotTrack()) {
+    return null;
+  }
+
+  return (
+    <div
+      className="banner"
+      aria-label="cookies banner"
+      role="region"
+      ref={banner}
+    >
+      {(submitted && (
+        <IcSectionContainer full-height aligned="full-width" tab-index="-1">
+          <IcTypography role="alert" variant="body">
+            You’ve {consentCookieApproved() ? "accepted" : "rejected"} analytics
+            cookies. You can{" "}
+            <IcLink href="/cookies-policy">change your cookie settings</IcLink>{" "}
+            at any time.
+          </IcTypography>
+          <div className="buttons">
+            <IcButton variant="primary" onClick={() => setVisible(false)}>
+              Hide cookie message
+            </IcButton>
+          </div>
+        </IcSectionContainer>
+      )) || (
+        <IcSectionContainer full-height aligned="full-width">
+          <IcTypography variant="h2">Cookies on this site</IcTypography>
+          <IcTypography variant="body">
+            We’d like to use analytics cookies to understand how you use the
+            Design System so that we can make improvements.
+          </IcTypography>
+          <div className="buttons">
+            <IcButton variant="primary" onClick={() => handleConsent(true)}>
+              Accept
+            </IcButton>
+            <IcButton variant="secondary" onClick={() => handleConsent(false)}>
+              Decline
+            </IcButton>
+            <IcLink href="/cookies-policy">Manage cookies</IcLink>
+          </div>
+        </IcSectionContainer>
+      )}
+    </div>
+  );
+};
+
+export default CookieBanner;
---
+++ src/content/static/components/InlineCookiesManager.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { IcSwitch } from "@ukic/react";
+import {
+  consentCookieApproved,
+  setConsent,
+} from "../../../components/CookieBanner/cookies.helper";
+
+const InlineCookiesManager: React.FC = () => {
+  const [approved, setApproved] = useState(consentCookieApproved());
+
+  const handleChange = () => {
+    setApproved(!approved);
+    setConsent(!approved);
+  };
+
+  document.addEventListener("icChange", handleChange);
+
+  return (
+    <>
+      <IcSwitch
+        checked={approved}
+        label="Optional analytics cookies"
+        showState
+      />
+    </>
+  );
+};
+
+export default InlineCookiesManager;
