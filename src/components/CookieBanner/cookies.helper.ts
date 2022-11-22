76272794f5f890b7ace572444e3cae8b0ab140c8
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2022-11-22 06:46:49 -0800

    fix(root): delete GA cookie that appears after consent has been revoked

--- src/components/CookieBanner/cookies.helper.ts
+++ src/components/CookieBanner/cookies.helper.ts
@@ -8,6 +8,10 @@ export const setCookie = (value: string) => {
   document.cookie = `${value}; expires=${date.toUTCString()}; path=/;`;
 };
 
+export const deleteCookie = (name: string) => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;`;
+};
+
 export const consentCookieActioned = () =>
   document.cookie.indexOf("ICDSPREF") !== -1;
 
