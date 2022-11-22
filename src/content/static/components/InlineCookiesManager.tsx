52fe207079ed1d30bcef318ee143698d9c2ba55f
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2022-11-22 06:46:49 -0800

    fix(root): delete GA cookie that appears after consent has been revoked

--- src/content/static/components/InlineCookiesManager.tsx
+++ src/content/static/components/InlineCookiesManager.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { IcSwitch } from "@ukic/react";
 import {
   consentCookieApproved,
+  deleteCookie,
   setConsent,
 } from "../../../components/CookieBanner/cookies.helper";
 
@@ -12,6 +13,10 @@ const InlineCookiesManager: React.FC = () => {
     setApproved(!approved);
     setConsent(!approved);
   };
+
+  if (!approved) {
+    deleteCookie("_ga_L8G7D6EHQB");
+  }
 
   return (
     <IcSwitch
