856de8a62eb4c94812211bfcb5659580073e3bbc
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2022-11-28 10:11:06 +0000

    feat(root): added environment var to branches workflow
    Added google search console token to branches workflow

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -18,7 +18,6 @@ const {
   VERSION,
   FOOTER_PROPS,
   META_DESCRIPTION,
-  GOOGLE_SEARCH_TOKEN,
   siteUrl,
 } = require("../../config");
 
@@ -67,12 +66,11 @@ const Layout: React.FC<LayoutProps> = ({
   description = "",
   children,
 }) => {
-
   let canonicalUrl: string = "";
-    if (typeof window !== "undefined") {
-      canonicalUrl = siteUrl + window.location.pathname;
+  if (typeof window !== "undefined") {
+    canonicalUrl = siteUrl + window.location.pathname;
   }
-  
+
   // Destructing GATSBY_ env variables client side returns an empty value. Full reference required.
   // eslint-disable-next-line prefer-destructuring
   const GATSBY_GA_TRACKING_ID = process.env.GATSBY_GA_TRACKING_ID;
@@ -81,118 +79,131 @@ const Layout: React.FC<LayoutProps> = ({
 
   // document object is not available during SSR so a check is in place.
   // document.cookie is required for consentCookieApproved method
-  if (typeof document !== 'undefined') {
+  if (typeof document !== "undefined") {
     defaultCookieConsentValue = consentCookieApproved();
   } else {
     defaultCookieConsentValue = false;
   }
 
-  const [cookieConsent, setCookieConsent] = useState<boolean>(defaultCookieConsentValue);
+  const [cookieConsent, setCookieConsent] = useState<boolean>(
+    defaultCookieConsentValue
+  );
 
   const handleCookieConsent = (consent: boolean) => {
     setCookieConsent(true);
     updateConsent(consent);
-  }
+  };
 
-  const value = React.useMemo(() => ({
-    cookieConsent, handleCookieConsent
-  }), [cookieConsent, handleCookieConsent])
+  const value = React.useMemo(
+    () => ({
+      cookieConsent,
+      handleCookieConsent,
+    }),
+    [cookieConsent, handleCookieConsent]
+  );
 
   const updateConsent = (consent: boolean) => {
-    if (typeof window !== 'undefined') {
-      window.gtag('consent', 'update', {
-        ad_storage: consent ? 'granted' : 'denied',
-        analytics_storage: consent ? 'granted' : 'denied',
+    if (typeof window !== "undefined") {
+      window.gtag("consent", "update", {
+        ad_storage: consent ? "granted" : "denied",
+        analytics_storage: consent ? "granted" : "denied",
       });
     }
-  }
+  };
 
   return (
-  <>
-    <Helmet>
-      <html lang="en" />
-      <title>{title || TITLE} - Intelligence Community Design System</title>
-      <meta
-        property="og:title"
-        content={`${title || TITLE} - Intelligence Community Design System`}
-      />
-      <meta property="og:type" content="website" />
-      <meta
-        name="description"
-        property="og:description"
-        content={`${description || META_DESCRIPTION}`}
-      />
-      <link rel="canonical" href={`${canonicalUrl}`} />
-      <meta property="og:url" content={`${canonicalUrl}`} />
-      <meta
-        property="og:image"
-        content="https://github.com/mi6/ic-design-system/blob/main/src/assets/og-image.png"
-      />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta name="google-site-verification" content={GOOGLE_SEARCH_TOKEN} />
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GATSBY_GA_TRACKING_ID}`} />
-      <script>
-        {`
+    <>
+      <Helmet>
+        <html lang="en" />
+        <title>{title || TITLE} - Intelligence Community Design System</title>
+        <meta
+          property="og:title"
+          content={`${title || TITLE} - Intelligence Community Design System`}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          name="description"
+          property="og:description"
+          content={`${description || META_DESCRIPTION}`}
+        />
+        <link rel="canonical" href={`${canonicalUrl}`} />
+        <meta property="og:url" content={`${canonicalUrl}`} />
+        <meta
+          property="og:image"
+          content="https://github.com/mi6/ic-design-system/blob/main/src/assets/og-image.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GATSBY_GA_TRACKING_ID}`}
+        />
+        <script>
+          {`
             window.dataLayer = window.dataLayer || [  ];
             function gtag(){dataLayer.push(arguments);}
             gtag('consent', 'default', {
-              'ad_storage': '${cookieConsent ? 'granted' : 'denied'}',
-              'analytics_storage': '${cookieConsent ? 'granted' : 'denied'}',
+              'ad_storage': '${cookieConsent ? "granted" : "denied"}',
+              'analytics_storage': '${cookieConsent ? "granted" : "denied"}',
             });
             gtag('js', new Date());
             gtag('config', '${GATSBY_GA_TRACKING_ID}');
         `}
-      </script>
-    </Helmet>
-    <CookieConsentContext.Provider value={value}>
-      <ClientOnly>
-        {title !== "Cookies Policy" && <CookieBanner />}
-        <div className="main-page-container">
-          <IcLink href="#main" id="skip" className="skip-content-link">
-            Skip to main content
-          </IcLink>
-          <TopNavWrapper appTitle={TITLE} status={STATUS} version={VERSION} />
-          <main id="main" className="homepage-wrapper">
-            {children}
-            {!contentProps?.index && <IcBackToTop target="main" />}
-          </main>
-        </div>
-        <div className="footer">
-          <IcFooter
-            description={FOOTER_PROPS.content}
-            caption={FOOTER_PROPS.caption}
-          >
-            {FOOTER_PROPS.footerLinks.map((footerLinks: FooterLinks) => (
-              <IcFooterLink
-                slot="link"
-                href={withPrefix(footerLinks.link)}
-                key={footerLinks.key}
-              >
-                {footerLinks.text}
-              </IcFooterLink>
-            ))}
-            <div slot="logo" className="logo-wrapper">
-              <IcFooterLink href="https://sis.gov.uk">
-                <SISLogo aria-labelledby="SIS Logo" />
-                <span className="link-text">Go to SIS website</span>
-              </IcFooterLink>
-              <IcFooterLink href="https://www.mi5.gov.uk">
-                <MI5Logo aria-labelledby="MI5 Logo" />
-                <span className="link-text">Go to MI5 website</span>
-              </IcFooterLink>
-              <IcFooterLink href="https://gchq.gov.uk">
-                <GCHQLogo aria-labelledby="GCHQ Logo" />
-                <span className="link-text">Go to GCHQ website</span>
-              </IcFooterLink>
-            </div>
-          </IcFooter>
-        </div>
-      </ClientOnly>
-    </CookieConsentContext.Provider>
-    <RouteAnnouncer page={`${title || ""} - ${TITLE}`} />
-  </>
-)};
+        </script>
+        <meta
+          name="google-site-verification"
+          content={process.env.GATSBY_GOOGLE_SEARCH_TOKEN}
+        />
+      </Helmet>
+      <CookieConsentContext.Provider value={value}>
+        <ClientOnly>
+          {title !== "Cookies Policy" && <CookieBanner />}
+          <div className="main-page-container">
+            <IcLink href="#main" id="skip" className="skip-content-link">
+              Skip to main content
+            </IcLink>
+            <TopNavWrapper appTitle={TITLE} status={STATUS} version={VERSION} />
+            <main id="main" className="homepage-wrapper">
+              {children}
+              {!contentProps?.index && <IcBackToTop target="main" />}
+            </main>
+          </div>
+          <div className="footer">
+            <IcFooter
+              description={FOOTER_PROPS.content}
+              caption={FOOTER_PROPS.caption}
+            >
+              {FOOTER_PROPS.footerLinks.map((footerLinks: FooterLinks) => (
+                <IcFooterLink
+                  slot="link"
+                  href={withPrefix(footerLinks.link)}
+                  key={footerLinks.key}
+                >
+                  {footerLinks.text}
+                </IcFooterLink>
+              ))}
+              <div slot="logo" className="logo-wrapper">
+                <IcFooterLink href="https://sis.gov.uk">
+                  <SISLogo aria-labelledby="SIS Logo" />
+                  <span className="link-text">Go to SIS website</span>
+                </IcFooterLink>
+                <IcFooterLink href="https://www.mi5.gov.uk">
+                  <MI5Logo aria-labelledby="MI5 Logo" />
+                  <span className="link-text">Go to MI5 website</span>
+                </IcFooterLink>
+                <IcFooterLink href="https://gchq.gov.uk">
+                  <GCHQLogo aria-labelledby="GCHQ Logo" />
+                  <span className="link-text">Go to GCHQ website</span>
+                </IcFooterLink>
+              </div>
+            </IcFooter>
+          </div>
+        </ClientOnly>
+      </CookieConsentContext.Provider>
+      <RouteAnnouncer page={`${title || ""} - ${TITLE}`} />
+    </>
+  );
+};
 
 export default Layout;
