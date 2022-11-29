e6d9867b9604139a91997779301ea0b0abbafd0e
jd - Legend <116016939+jd239@users.noreply.github.com>
2022-11-29 20:48:56 +0000

    feat(root): added responsive cookies table
    Built responsive cookies table which switches from cards in xs to md to a table from lg and greater

---
+++ src/content/static/components/CookiesData/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { debounce } from "../../../../utils/helpers";
+
+import CookiesCards from "./CookiesCards";
+import CookiesTable from "./CookiesTable";
+
+export interface CookiesDataProps {
+  headers: string[];
+  data: CookiesDetails[];
+  caption: string
+}
+
+export interface CookiesDetails {
+  provider: string;
+  domain: string;
+  desc: string;
+  cookies: string;
+}
+
+const CookiesData = ({ headers, data, caption }: CookiesDataProps) => {
+  let defaultViewportWidth = 0;
+  const props = {
+    headers,
+    data,
+    caption
+  }
+
+  if (typeof window !== "undefined") {
+    defaultViewportWidth = window.innerWidth;
+  }
+
+  const [viewportWidth, setViewportWidth] = useState(defaultViewportWidth);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      debounce(() => setViewportWidth(window.innerWidth))
+    );
+  }, []);
+
+  if (viewportWidth > 991) {
+    return <CookiesTable {...props} />;
+  }
+
+  return <CookiesCards {...props} />;
+};
+
+export default CookiesData;
