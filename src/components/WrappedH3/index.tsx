9db163e84c8e24b562359d6e057f300990115dfa
MI6-27 <90763452+MI6-27@users.noreply.github.com>
2022-12-13 15:05:19 +0000

    Revert "fix(root): improve permalink layout and usability"
    This reverts commit e678b2375258bc7c746f492ee5dec9ae89f5c913.

--- src/components/WrappedH3/index.tsx
+++ src/components/WrappedH3/index.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 
 import { IcTypography } from "@ukic/react";
-import Permalink from "../Permalink";
-
-const { slug } = require("github-slugger");
 
 import "./index.css";
 
 const WrappedH3: React.FC = (props: any) => (
   <IcTypography variant="h3" applyVerticalMargins className="h3">
     {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
-    <h5 id={slug(props.children)}>
-      {props.children}
-      <Permalink title={props.children} sluggedTitle={slug(props.children)} />
-    </h5>
+    <h5 {...props} />
   </IcTypography>
 );
 
