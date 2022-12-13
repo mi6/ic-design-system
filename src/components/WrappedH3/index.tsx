b382381b14bbfa47da0d7a76b2800abe26c4ec73
MI6-27 <90763452+MI6-27@users.noreply.github.com>
2022-12-13 15:12:55 +0000

    fix(root): improve permalink layout and usability
    added tooltip to permalink, improved position within headings and changed to an iconbutton
    appearance. removed dependency.
    
    fix #24

--- src/components/WrappedH3/index.tsx
+++ src/components/WrappedH3/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 import { IcTypography } from "@ukic/react";
+import Permalink from "../Permalink";
 
 import "./index.css";
 
-const WrappedH3: React.FC = (props: any) => (
+const { slug } = require("github-slugger");
+
+interface WrappedH3Props {
+  children: string;
+}
+
+const WrappedH3: React.FC<WrappedH3Props> = ({ children }) => (
   <IcTypography variant="h3" applyVerticalMargins className="h3">
     {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
-    <h5 {...props} />
+    <h5 id={slug(children)}>
+      {children}
+      <Permalink title={children?.toString()} sluggedTitle={slug(children)} />
+    </h5>
   </IcTypography>
 );
 
