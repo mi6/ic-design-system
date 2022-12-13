830b1fbb186f1ebcde554c3ff6f5c27f78c2a19c
MI6-27 <90763452+MI6-27@users.noreply.github.com>
2022-12-13 14:57:18 +0000

    fix(root): improve permalink layout and usability
    added tooltip to permalink, improved position within headings and changed to appear as an icon
    button. required removing gatsby plugin and using bespoke functionality.
    
    fix #24

--- src/components/WrappedH2/index.tsx
+++ src/components/WrappedH2/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 import { IcTypography } from "@ukic/react";
+import Permalink from "../Permalink";
+
+const { slug } = require("github-slugger");
 
 import "./index.css";
 
 const WrappedH2: React.FC = (props: any) => (
   <IcTypography variant="h2" applyVerticalMargins className="h2">
     {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
-    <h4 {...props} />
+    <h4 id={slug(props.children)}>
+      {props.children}
+      <Permalink title={props.children} sluggedTitle={slug(props.children)} />
+    </h4>
   </IcTypography>
 );
 
