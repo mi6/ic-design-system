bb9f3ebf7c3a5c1c1a0121150f865b9eb5e5c053
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2023-02-02 13:42:29 +0000

    fix(root): add deprecated event descriptions back
    Add deprecation descriptions and warning status tag to events and props table.
    
    . #269

--- src/components/ComponentDetails/PropTable/PropDescription/index.tsx
+++ src/components/ComponentDetails/PropTable/PropDescription/index.tsx
@@ -6,13 +6,22 @@ const PropDescription: React.FC<{
   description: string;
   type: string;
   required: boolean;
-}> = ({ description, type, required }) => (
+  deprecation: string | undefined;
+}> = ({ description, type, required, deprecation }) => (
   <div className="prop-description">
     <ic-typography>{description}</ic-typography>
     {required && (
       <ic-typography variant="subtitle-small">Required</ic-typography>
     )}
     <CodeAttribute label={`type: ${type}`} />
+    {deprecation && (
+      <>
+        <ic-typography>{deprecation}</ic-typography>
+        <div className="deprecation-tag">
+          <ic-status-tag label="Deprecated" status="warning" small />
+        </div>
+      </>
+    )}
   </div>
 );
 
