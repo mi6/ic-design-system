58dde58bce936b080729e4ce6e2aa573d77dae39
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2023-02-03 03:08:23 -0800

    docs(root): refactor PropName component so it can be used to display React events in event table
    Rename PropName component to AttributeName so it can be re-used in the events table to display web
    component and React events. Simplify the component using a map
    
    . #261

---
+++ src/components/ComponentDetails/AttributeName/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+const AttributeName: React.FC<{
+  name: string[];
+  value: (string | undefined)[];
+}> = ({ name, value }) => (
+  <div className="attribute-name">
+    {name.map((attributeName, i) => (
+      <div key={`attribute-${attributeName.toLowerCase().split(" ")[0]}`}>
+        <ic-typography variant="subtitle-small">{attributeName}</ic-typography>
+        <ic-typography variant="subtitle-large">{value[i]}</ic-typography>
+      </div>
+    ))}
+  </div>
+);
+
+export default AttributeName;
