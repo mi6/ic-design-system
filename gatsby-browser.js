8953e67238b666dcbdc4751f0d9a4dab3a98fe5f
gd2910 <122624946+gd2910@users.noreply.github.com>
2023-06-01 16:08:00 +0000

    fix(root): see internal issue 2336
    See internal issue 2336

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@ import "@ukic/web-components/dist/core/normalize.css";
 import "./src/styles/gatsby-reset.css";
 import "./src/styles/gatsby-override.css";
 
+import paths from "./src/utils/paths";
 import React from "react";
 import Layout from "./src/components/Layout";
 
@@ -12,9 +13,17 @@ const { defineCustomElements } = require("@ukic/web-components/loader");
 defineCustomElements();
 
 // eslint-disable-next-line import/prefer-default-export
-export const wrapPageElement = ({ element, props }) => (
-  <Layout {...props}>{element}</Layout>
-);
+export const wrapPageElement = ({ element, props }) => {
+  if (
+    paths.some((el) => {
+      return props.path === el;
+    })
+  ) {
+    return <>{element}</>;
+  } else {
+    return <Layout {...props}>{element}</Layout>;
+  }
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const onInitialClientRender = () => {
