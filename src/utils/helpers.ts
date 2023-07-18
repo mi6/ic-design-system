6221b80fdf853be491ea46a65dbe4f80adf24247
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2023-07-18 10:28:24 +0100

    fix(root): fixed table width issues on mobile
    adjusted table css to not go off screen on mobile devices. also fixed lint warning

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export function debounce(fn: Function, timeout = 300) {
   let timer: ReturnType<typeof setTimeout>;
-  return function (this: any, ...args: any[]) {
+  return function func(this: any, ...args: any[]) {
     clearTimeout(timer);
     timer = setTimeout(() => {
       fn.apply(this, args);
