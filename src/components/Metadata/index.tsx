a8678c3ea9e1a2409350f26a23be47116e647e90
MI6-255 <90764648+MI6-255@users.noreply.github.com>
2023-12-13 15:45:51 +0000

    docs(root): remove "Edit this page" from all pages
    remove "Edit this page" from all changes to prevent users getting confused and breaking the commits

--- src/components/Metadata/index.tsx
+++ src/components/Metadata/index.tsx
@@ -3,38 +3,17 @@ import dateFormat from "dateformat";
 
 import "./index.css";
 
-interface ContributeLinkProps {
-  contribute?: string;
-}
-
 interface MetadataProps {
   publishDate?: string;
-  contribute?: string;
 }
-
-const ContributeLink: React.FC<ContributeLinkProps> = ({ contribute = "" }) => {
-  const link = process.env.GATSBY_SOURCE_REPO_LINK
-    ? process.env.GATSBY_SOURCE_REPO_LINK
-    : contribute;
-  return contribute ? (
-    <>
-      <ic-link href={link} rel="noopener noreferrer" target="_blank">
-        Edit this page
-      </ic-link>
-      .
-    </>
-  ) : null;
-};
 
 const Metadata: React.FC<MetadataProps> = ({
   publishDate = null,
-  contribute = "",
 }) => (
   <div>
     <hr className="bottom-line" />
     <ic-section-container>
       <ic-typography variant="label">
-        <ContributeLink contribute={contribute} />
         {publishDate && publishDate.length >= 0 ? (
           <> Last reviewed {dateFormat(publishDate, "d mmmm yyyy")}.</>
         ) : null}
