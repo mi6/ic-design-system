1f926e4b2e89bf5b5e260437864f3d1c45f5ecfc
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-01-23 17:33:54 +0000

    feat(root): add top & side nav patterns code examples, add Stackblitz functionality for patterns
    Add top navigation and side navigation pattern code examples for React and web components,
    compatible with Javascript. Add stackblitz button functionality along with helper functions
    
    . #763 #760 #764 #765 #758 #751

---
+++ src/content/structured/patterns/components/StackblitzButton/index.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import sdk from "@stackblitz/sdk";
+import {
+  createIndexTsx,
+  createReactIndexHTML,
+  createWebComponentsIndexHTML,
+  packageJson,
+  tsConfig,
+  viteConfig,
+} from "./stackblitz-helpers";
+import { StackBlitzLogo } from "../../../../../assets/svg";
+import { useViewportWidth } from "../../../../../utils/helpers";
+
+export type StackblitzProps = {
+  codeSnippet?: string;
+  isWebComponents?: boolean;
+  projectTitle: string;
+  projectDescription?: string;
+};
+
+const StackblitzButton: React.FC<StackblitzProps> = ({
+  codeSnippet,
+  isWebComponents,
+  projectTitle,
+  projectDescription,
+}) => {
+  const viewportWidth = useViewportWidth();
+  const isLargeViewport: boolean = viewportWidth > 992;
+
+  const createStackblitzProject = () => {
+    const files: { [key: string]: string } = {};
+    const isWebComponentsInternal: boolean =
+      isWebComponents !== undefined ? isWebComponents : true;
+    const isJSX: boolean = true;
+    // Add functionality to manage whether Stackblitz project should use TypeScript or JavaScript
+    const ext = isJSX ? "jsx" : "tsx";
+
+    if (codeSnippet !== undefined && codeSnippet !== "") {
+      if (isWebComponents) {
+        files[`index.html`] = projectTitle.includes("pattern")
+          ? codeSnippet
+          : createWebComponentsIndexHTML(codeSnippet);
+      } else {
+        files[`src/index.${ext}`] = codeSnippet;
+      }
+    }
+
+    // Define the index.tsx content for a React app
+    const indexTsx = createIndexTsx(projectTitle, true);
+
+    // Change file structure for React code examples
+    if (
+      (isWebComponents !== undefined && !isWebComponents) ||
+      !isWebComponentsInternal
+    ) {
+      files[`src/app.${ext}`] = files[`src/index.${ext}`];
+      files[`src/index.${ext}`] = indexTsx;
+      files[`index.html`] = createReactIndexHTML(ext);
+      files["vite.config.js"] = viteConfig;
+      if (ext === "tsx") {
+        files["tsconfig.json"] = tsConfig;
+      }
+    }
+
+    files["package.json"] = JSON.stringify(
+      packageJson(projectTitle, isWebComponentsInternal, ext),
+      null,
+      2
+    );
+
+    const description =
+      projectDescription === undefined || projectDescription === ""
+        ? ""
+        : projectDescription;
+
+    sdk.openProject(
+      {
+        title: `ICDS ${projectTitle}`,
+        description,
+        files,
+        template: "node",
+      },
+      {
+        openFile: isWebComponentsInternal ? [`index.html`] : [`src/app.${ext}`],
+      }
+    );
+  };
+
+  return (
+    <ic-button
+      aria-label="Open code example in Stackblitz"
+      size={isLargeViewport ? "small" : "default"}
+      variant={isLargeViewport ? "tertiary" : "icon"}
+      appearance="dark"
+      onClick={() => createStackblitzProject()}
+    >
+      {isLargeViewport && (
+        <>
+          <span slot="left-icon">
+            <StackBlitzLogo />
+          </span>
+          StackBlitz
+        </>
+      )}
+      {!isLargeViewport && <StackBlitzLogo />}
+    </ic-button>
+  );
+};
+
+export default StackblitzButton;
