20b4a0d0f0aea94830f0710a799d3ffb2d1f2b78
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-01-23 17:33:54 +0000

    feat(root): add top & side nav patterns code examples, add Stackblitz functionality for patterns
    Add top navigation and side navigation pattern code examples for React and web components,
    compatible with Javascript. Add stackblitz button functionality along with helper functions
    
    . #763 #760 #764 #765 #758 #751

--- src/components/CodePreview/index.tsx
+++ src/components/CodePreview/index.tsx
@@ -1,23 +1,35 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
-import React, { CSSProperties, ReactNode } from "react";
-
+import React, {
+  CSSProperties,
+  ReactNode,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import startCase from "lodash.startcase";
 import {
   mdiCheckboxMarkedCircle,
   mdiCloseCircle,
   mdiContentCopy,
+  mdiMenuDown,
+  mdiMenuUp,
 } from "@mdi/js";
 import Icon from "@mdi/react";
 import clsx from "clsx";
 import { SlottedSVG } from "@ukic/react";
-
+import StackblitzButton, {
+  StackblitzProps,
+} from "../../content/structured/patterns/components/StackblitzButton";
+import PageMetadataContext from "../../context/PageMetadata";
 import "./index.css";
+import { useViewportWidth } from "../../utils/helpers";
 
 interface Snippet {
   language: string;
   snippet: string;
 }
 
-interface ComponentPreviewProps {
+interface ComponentPreviewProps extends Partial<StackblitzProps> {
   snippets?: Snippet[];
   left?: boolean;
   noPadding?: boolean;
@@ -25,42 +37,107 @@ interface ComponentPreviewProps {
   children: ReactNode;
   style: CSSProperties;
   state: "none" | "good" | "bad";
+  type?: string;
 }
 
-const CodeSnippet: React.FC<{ code: string }> = ({ code }) => (
-  <>
-    <Highlight {...defaultProps} code={code} language="jsx" theme={undefined}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={clsx(className, "snippet")} style={style}>
-          <code>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
+interface CodeSnippetProps extends Partial<StackblitzProps> {
+  code: string;
+  type?: string;
+  show: boolean;
+  setShow: Dispatch<SetStateAction<boolean>>;
+  showStackblitzBtn?: boolean;
+}
+
+const CodeSnippet: React.FC<CodeSnippetProps> = ({
+  code,
+  isWebComponents,
+  type,
+  show,
+  setShow,
+  showStackblitzBtn,
+  projectTitle,
+  projectDescription,
+}) => {
+  const viewportWidth = useViewportWidth();
+  const isLargeViewport: boolean = viewportWidth > 992;
+
+  return (
+    <>
+      {show && (
+        <Highlight
+          {...defaultProps}
+          code={code}
+          language="jsx"
+          theme={undefined}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={clsx(className, "snippet")} style={style}>
+              <code>
+                {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
                 ))}
-              </div>
-            ))}
-          </code>
-        </pre>
+              </code>
+            </pre>
+          )}
+        </Highlight>
       )}
-    </Highlight>
-    <div className="snippet-container">
-      <ic-button
-        variant="tertiary"
-        appearance="dark"
-        onClick={() => {
-          navigator.clipboard.writeText(code);
-          document
-            .querySelector<HTMLIcToastElement>("#copy-to-clipboard-toast")
-            ?.setVisible();
-        }}
-      >
-        Copy code
-        <SlottedSVG path={mdiContentCopy} slot="left-icon" />
-      </ic-button>
-    </div>
-  </>
-);
+      <div className={clsx(type === "pattern" && type, "snippet-container")}>
+        {type === "pattern" && (
+          <ic-button
+            variant="tertiary"
+            size={isLargeViewport ? "small" : "default"}
+            appearance="dark"
+            onClick={() => setShow(!show)}
+          >
+            {!show ? "Show" : "Hide"} code
+            <SlottedSVG
+              slot="right-icon"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              path={!show ? mdiMenuDown : mdiMenuUp}
+            />
+          </ic-button>
+        )}
+        <div className="code-actions">
+          {showStackblitzBtn && projectTitle !== undefined && (
+            <StackblitzButton
+              codeSnippet={code}
+              isWebComponents={isWebComponents}
+              projectTitle={projectTitle}
+              projectDescription={projectDescription}
+            />
+          )}
+          <ic-button
+            aria-label={isLargeViewport ? "" : "Copy code"}
+            variant={isLargeViewport ? "tertiary" : "icon"}
+            size={isLargeViewport ? "small" : "default"}
+            appearance="dark"
+            onClick={() => {
+              navigator.clipboard.writeText(code);
+              document
+                .querySelector<HTMLIcToastElement>("#copy-to-clipboard-toast")
+                ?.setVisible();
+            }}
+          >
+            <SlottedSVG
+              path={mdiContentCopy}
+              slot={isLargeViewport ? "left-icon" : undefined}
+              viewBow="0 0 24 24"
+              width="24"
+              height="24"
+            />
+            {isLargeViewport && "Copy code"}
+          </ic-button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 const ComponentPreview: React.FC<ComponentPreviewProps> = ({
   snippets,
@@ -70,53 +147,90 @@ const ComponentPreview: React.FC<ComponentPreviewProps> = ({
   centered = true,
   style,
   state = "none",
-}) => (
-  <div className="comp-preview">
-    <h4 className="offscreen">Interactive example</h4>
-    <div
-      className={clsx(
-        "comp-zone",
-        centered && "centered",
-        left && "left",
-        noPadding && "no-padding"
-      )}
-      style={style}
-    >
-      {(state === "good" && (
-        <Icon
-          path={mdiCheckboxMarkedCircle}
-          size="36px"
-          className={clsx("comp-icon", "comp-do-icon")}
-          aria-hidden="true"
-        />
-      )) ||
-        (state === "bad" && (
+  projectTitle,
+  projectDescription,
+  type,
+}) => {
+  const [show, setShow] = useState<boolean>(type !== "pattern");
+
+  const pageMetadata = React.useContext(PageMetadataContext);
+
+  const getTypeOfProject = (snippet: Snippet) => {
+    if (type === "pattern") {
+      return " pattern";
+    }
+    if (snippet.language === "React") {
+      return " component";
+    }
+    return "";
+  };
+
+  return (
+    <div className="comp-preview">
+      <h4 className="offscreen">Interactive example</h4>
+      <div
+        className={clsx(
+          "comp-zone",
+          centered && "centered",
+          left && "left",
+          noPadding && "no-padding",
+          !!type && type
+        )}
+        style={style}
+      >
+        {(state === "good" && (
           <Icon
-            path={mdiCloseCircle}
+            path={mdiCheckboxMarkedCircle}
             size="36px"
-            className={clsx("comp-icon", "comp-dont-icon")}
+            className={clsx("comp-icon", "comp-do-icon")}
             aria-hidden="true"
           />
-        ))}
-      {children}
+        )) ||
+          (state === "bad" && (
+            <Icon
+              path={mdiCloseCircle}
+              size="36px"
+              className={clsx("comp-icon", "comp-dont-icon")}
+              aria-hidden="true"
+            />
+          ))}
+        {children}
+      </div>
+      {snippets && (
+        <ic-tab-context>
+          <div className="link-zone">
+            <ic-tab-group inline label="Framework code snippets">
+              {snippets.map((snippet, index) => (
+                <ic-tab tab-position={index}>{snippet.language}</ic-tab>
+              ))}
+            </ic-tab-group>
+          </div>
+          {snippets.map((snippet, index) => (
+            <ic-tab-panel tab-position={index}>
+              <CodeSnippet
+                type={type}
+                code={snippet.snippet}
+                show={show}
+                setShow={setShow}
+                showStackblitzBtn={
+                  !!process.env.GATSBY_GA_TRACKING_ID && type === "pattern"
+                }
+                isWebComponents={snippet.language === "Web component"}
+                projectTitle={`${
+                  projectTitle || startCase(pageMetadata.pageTitle)
+                } (${snippet.language}${getTypeOfProject(snippet)})`}
+                projectDescription={
+                  projectDescription === undefined || projectDescription === ""
+                    ? undefined
+                    : projectDescription
+                }
+              />
+            </ic-tab-panel>
+          ))}
+        </ic-tab-context>
+      )}
     </div>
-    {snippets && (
-      <ic-tab-context>
-        <div className="link-zone">
-          <ic-tab-group inline label="Framework code snippets">
-            {snippets.map((snippet, index) => (
-              <ic-tab tab-position={index}>{snippet.language}</ic-tab>
-            ))}
-          </ic-tab-group>
-        </div>
-        {snippets.map((snippet, index) => (
-          <ic-tab-panel tab-position={index}>
-            <CodeSnippet code={snippet.snippet} />
-          </ic-tab-panel>
-        ))}
-      </ic-tab-context>
-    )}
-  </div>
-);
+  );
+};
 
 export default ComponentPreview;
---
+++ src/pages/side-navigation-pattern.tsx
@@ -0,0 +1,278 @@
+import React from "react";
+import { PageProps } from "gatsby";
+import {
+  IcClassificationBanner,
+  IcDivider,
+  IcFooter,
+  IcFooterLink,
+  IcNavigationItem,
+  IcSectionContainer,
+  IcSideNavigation,
+  IcTypography,
+  SlottedSVG,
+} from "@ukic/react";
+import { IcAlignment } from "@ukic/web-components";
+import { createUseStyles } from "react-jss";
+import {
+  mdiCompassOutline,
+  mdiHomeOutline,
+  mdiMagnify,
+  mdiChartTimelineVariant,
+  mdiCommentTextOutline,
+  mdiNumeric1BoxOutline,
+  mdiNumeric2BoxOutline,
+  mdiNumeric3BoxOutline,
+  mdiNumeric4BoxOutline,
+  mdiCogOutline,
+  mdiHuman,
+  mdiWeb,
+} from "@mdi/js";
+
+const SideNavigationPattern: React.FC<PageProps> = () => {
+  const useStyles = createUseStyles({
+    contentDivContainer: {
+      display: "flex",
+      flexDirection: "column",
+      flexGrow: "1",
+    },
+    mainContentDiv: {
+      border: "var(--ic-border-width) dashed var(--ic-architectural-400)",
+      padding: "var(--ic-space-md)",
+      flex: 1,
+    },
+    parentDiv: { display: "flex", height: "100%" },
+    main: { minHeight: "100vh", display: "flex" },
+    mainSectionContainer: { display: "flex", flex: 1 },
+  });
+
+  const classes = useStyles();
+
+  const alignment: IcAlignment = "center";
+
+  return (
+    <div id="side-nav-pattern-root">
+      <div className={classes.parentDiv}>
+        <IcClassificationBanner />
+        <IcSideNavigation
+          appTitle="[Enter your application name]"
+          status="alpha"
+          version="v0.0.7"
+        >
+          <SlottedSVG
+            slot="app-icon"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            path={mdiCompassOutline}
+          />
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+            selected
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiHomeOutline}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiMagnify}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiChartTimelineVariant}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiCommentTextOutline}
+            />
+          </IcNavigationItem>
+          <IcDivider slot="primary-navigation" />
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiNumeric1BoxOutline}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiNumeric2BoxOutline}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiNumeric3BoxOutline}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="primary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiNumeric4BoxOutline}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="secondary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiCogOutline}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="secondary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiHuman}
+            />
+          </IcNavigationItem>
+          <IcNavigationItem
+            slot="secondary-navigation"
+            label="[navigationOption]"
+            href="#"
+          >
+            <SlottedSVG
+              slot="icon"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              path={mdiWeb}
+            />
+          </IcNavigationItem>
+        </IcSideNavigation>
+        <div className={classes.contentDivContainer}>
+          <main className={classes.main}>
+            <IcSectionContainer
+              aligned={alignment}
+              className={classes.mainSectionContainer}
+            >
+              <div className={classes.mainContentDiv}>
+                <IcTypography variant="h2">
+                  <h2>Example heading</h2>
+                </IcTypography>
+                <IcTypography variant="subtitle-large">
+                  <p>Example sub-heading</p>
+                </IcTypography>
+                <IcTypography>
+                  <p>
+                    Remove this div and add your custom content in
+                    IcSectionContainer.
+                  </p>
+                </IcTypography>
+              </div>
+            </IcSectionContainer>
+          </main>
+          <IcFooter
+            aligned={alignment}
+            caption="This information is exempt under the Freedom of Information Act 2000 (FOIA) and may be exempt under other UK information legislation."
+            description="This pattern was built using the Intelligence Community Design
+            System (ICDS)."
+          >
+            <IcFooterLink slot="link" href="">
+              [footerLink]
+            </IcFooterLink>
+            <IcFooterLink slot="link" href="">
+              [footerLink]
+            </IcFooterLink>
+            <IcFooterLink slot="link" href="">
+              [footerLink]
+            </IcFooterLink>
+            <IcFooterLink slot="link" href="">
+              [footerLink]
+            </IcFooterLink>
+            <IcFooterLink slot="link" href="">
+              [footerLink]
+            </IcFooterLink>
+            <IcFooterLink slot="logo" href="/">
+              <SlottedSVG
+                xmlns="http://www.w3.org/2000/svg"
+                height="48px"
+                viewBox="0 0 24 24"
+                width="48px"
+                fill="#FFFFFF"
+              >
+                <path
+                  d="M12 6.19L17 10.69V18.5H15V12.5H9V18.5H7V10.69L12 6.19ZM12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L12 3.5Z"
+                  fill="currentColor"
+                />
+              </SlottedSVG>
+            </IcFooterLink>
+          </IcFooter>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNavigationPattern;
