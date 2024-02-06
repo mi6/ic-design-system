06fc1106bc3f3664782c1c8e032cd5b60414ab63
MI6-94 <90764530+MI6-94@users.noreply.github.com>
2024-02-06 16:09:00 +0000

    feat(root): aligned 404 page to use error state component
    Updated 404 page and removed custom CSS with use of error state component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,17 @@
 import React from "react";
+
 import { PageProps } from "gatsby";
 
-import fig404 from "../../static/404.png";
-
-import "./404.css";
-
 const NotFoundPage: React.FC<PageProps> = () => (
-  <ic-section-container aligned="full-width" id="container-404">
-    <img src={fig404} alt="" width="200" loading="lazy" />
-    <ic-typography variant="h1" apply-vertical-margins>
-      <h2>That page wasn&apos;t found</h2>
-    </ic-typography>
-    <ic-typography variant="body" apply-vertical-margins>
-      <p>
-        Sorry, we can&apos;t find the page you&apos;re looking for. This may be
-        because the page has moved or there was a typo in the URL.
-      </p>
-    </ic-typography>
-    <ic-typography variant="label" apply-vertical-margins>
-      <p>
-        Try <ic-link href="/">searching from the homepage</ic-link>.
-      </p>
-    </ic-typography>
+  <ic-section-container aligned="full-width">
+    <ic-empty-state
+      heading="That page wasn't found"
+      body=" This may be because the page has moved or there was a typo in the URL."
+    >
+      <ic-link slot="actions" href="/">
+        Return to home page
+      </ic-link>
+    </ic-empty-state>
   </ic-section-container>
 );
 
