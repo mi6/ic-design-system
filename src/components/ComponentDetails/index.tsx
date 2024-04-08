2c4a44292a61c9ec979eef2e9fe24744e00fec6b
gd2910 <122624946+gd2910@users.noreply.github.com>
2024-04-08 15:02:29 +0000

    docs(root): date-picker guidance
    date-picker guidance including package import for canary-docs and wrapper change on ComponentDetails
    to facilitate props tables etc for canary components

--- src/components/ComponentDetails/index.tsx
+++ src/components/ComponentDetails/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import componentJson from "@ukic/docs";
+import canaryComponentJson from "@ukic/canary-docs";
 import EventTable from "./EventTable";
 import MethodTable from "./MethodTable";
 import PropTable from "./PropTable";
 import SlotTable from "./SlotTable";
 import StyleTable from "./StyleTable";
 
-const ComponentDetails: React.FC<{ component: string }> = ({ component }) => {
-  const componentDetails = componentJson.components.find(
+const ComponentDetails: React.FC<{ component: string; canary?: boolean }> = ({
+  component,
+  canary,
+}) => {
+  const componentDetailsJson = canary ? canaryComponentJson : componentJson;
+  const componentDetails = componentDetailsJson.components.find(
     ({ tag }) => tag === component
   )!;
 
