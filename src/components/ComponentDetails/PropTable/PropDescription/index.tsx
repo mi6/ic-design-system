eb9c301533d3e58456610793af7d2d23158c2950
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-05-23 10:53:23 +0100

    docs(root): add type name to component prop table
    Add type name to component prop table
    
    . #982

--- src/components/ComponentDetails/PropTable/PropDescription/index.tsx
+++ src/components/ComponentDetails/PropTable/PropDescription/index.tsx
@@ -5,27 +5,31 @@ import { backtickToCodeBlock } from "../../../../utils/helpers";
 
 const PropDescription: React.FC<{
   description: string;
+  typeName: string;
   type: string;
   required: boolean;
   deprecation: string | undefined;
-}> = ({ description, type, required, deprecation }) => (
-  <div className="prop-description">
-    {!!description && (
-      <ic-typography>{backtickToCodeBlock(description)}</ic-typography>
-    )}
-    {required && (
-      <ic-typography variant="subtitle-small">Required</ic-typography>
-    )}
-    {!deprecation && <CodeAttribute label={`type: ${type}`} />}
-    {deprecation && (
-      <>
-        <ic-typography>{deprecation}</ic-typography>
-        <div className="deprecation-tag">
-          <ic-status-tag label="Deprecated" status="warning" small />
-        </div>
-      </>
-    )}
-  </div>
-);
+}> = ({ description, type, typeName, required, deprecation }) => {
+  const displayedType = typeName !== type ? `${typeName} - ${type}` : type;
+  return (
+    <div className="prop-description">
+      {!!description && (
+        <ic-typography>{backtickToCodeBlock(description)}</ic-typography>
+      )}
+      {required && (
+        <ic-typography variant="subtitle-small">Required</ic-typography>
+      )}
+      {!deprecation && <CodeAttribute label={`type: ${displayedType}`} />}
+      {deprecation && (
+        <>
+          <ic-typography>{deprecation}</ic-typography>
+          <div className="deprecation-tag">
+            <ic-status-tag label="Deprecated" status="warning" small />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default PropDescription;
