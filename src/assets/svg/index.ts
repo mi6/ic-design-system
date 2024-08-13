58d7a2fde26210c75cebd4fb042449ec8cebffaa
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-08-13 13:14:32 +0100

    feat(root): update homepage designs to match Figma for v3
    Update homepage designs to match Figma for v3
    
    . #963

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -4,3 +4,5 @@ export { default as MI5Logo } from "./mi5-logo.svg";
 export { default as SISLogo } from "./sis-logo.svg";
 export { default as ICDSLogo } from "./icds-logo.svg";
 export { default as StackBlitzLogo } from "./stackblitz-logo.svg";
+export { default as DesignImage } from "./designers-image.svg";
+export { default as DevImage } from "./developers-image.svg";
