418fc77e63bba856eed9c98bbb33cf386e386c82
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2025-03-26 11:06:27 +0000

    feat(root): add skip link guidance

---
+++ src/content/structured/components/skip-link/images/index.tsx
@@ -0,0 +1,6 @@
+export { default as Fig1DefaultVariantLight } from "./fig-1-use-default-variant-of-skip-link.png";
+export { default as Fig1DefaultVariantDark } from "./fig-1-use-default-variant-of-skip-link-dark.png";
+export { default as Fig2InlineVariantLight } from "./fig-2-use-inline-variant-in-navigation-component.png";
+export { default as Fig2InlineVariantDark } from "./fig-2-use-inline-variant-in-navigation-component-dark.png";
+export { default as Fig3NoHeaderContentLight } from "./fig-3-no-skip-link-when-not-needed.png";
+export { default as Fig3NoHeaderContentDark } from "./fig-3-no-skip-link-when-not-needed-dark.png";
