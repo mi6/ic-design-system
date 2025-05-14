d365366504e680969fe4d1c7c14ccc6efcc9b787
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2025-05-14 10:02:20 +0100

    docs(root): update data table guidance/accessibility pages
    Update data table guidance/accessibility pages to provide more information on usage
    
    . #1386

---
+++ src/content/structured/components/data-table/images/index.tsx
@@ -0,0 +1,12 @@
+export { default as Fig1DataTableLight } from "./fig-1-use-data-table-for-large-dataset.png";
+export { default as Fig1DataTableDark } from "./fig-1-use-data-table-for-large-dataset-dark.png";
+export { default as Fig2DataTableLight } from "./fig-2-use-a-title-bar-for-context.png";
+export { default as Fig2DataTableDark } from "./fig-2-use-a-title-bar-for-context-dark.png";
+export { default as Fig3DataTableLight } from "./fig-3-use-loading-state-for-passive-loading.png";
+export { default as Fig3DataTableDark } from "./fig-3-use-loading-state-for-passive-loading-dark.png";
+export { default as Fig4DataTableLight } from "./fig-4-use-updating-state-for-active-loading.png";
+export { default as Fig4DataTableDark } from "./fig-4-use-updating-state-for-active-loading-dark.png";
+export { default as Fig5DataTableLight } from "./fig-5-dont-use-tables-for-simple-information.png";
+export { default as Fig5DataTableDark } from "./fig-5-dont-use-tables-for-simple-information-dark.png";
+export { default as Fig6DataTableLight } from "./fig-6-dont-use-pagination-for-small-dataset.png";
+export { default as Fig6DataTableDark } from "./fig-6-dont-use-pagination-for-small-dataset-dark.png";
