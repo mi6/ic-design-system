171c58358e6e0b49bb927be197f2538a03beb5c0
MI6-255 <90764648+MI6-255@users.noreply.github.com>
2025-06-09 14:13:59 +0000

    feat(root): migrate from Google Analytics to Matomo
    to provide a more privacy focused platform to the users
    
    1546

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,6 +3,6 @@ declare global {
   // Added to make gtag available for gtag update function with cookie consent
   // eslint-disable-next-line no-unused-vars
   interface Window {
-    gtag: Function;
+    _paq?: any[];
   }
 }
