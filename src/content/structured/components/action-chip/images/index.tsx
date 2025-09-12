8c3f1e42e607ed9bf956faa4d2b40b9aed1feda6
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2025-09-12 10:42:43 +0000

    docs(root): add guidance for Action Chip
    Added guidance pages for Action Chip, linking it in the existing Chip page
    Update Chip guidance according to latest internal docs, added dark mode videos
    
    ref #1613

---
+++ src/content/structured/components/action-chip/images/index.tsx
@@ -0,0 +1,8 @@
+export { default as Fig1ActionChipsLight } from "./fig-1-do-use-action-chips-to-display-smart-or-automated-actions.png";
+export { default as Fig1ActionChipsDark } from "./fig-1-do-use-action-chips-to-display-smart-or-automated-actions-dark.png";
+export { default as Fig2ActionChipsLight } from "./fig-2-use-action-chips-in-conjunction-with-other-chips.png";
+export { default as Fig2ActionChipsDark } from "./fig-2-use-action-chips-in-conjunction-with-other-chips-dark.png";
+export { default as Fig3ActionChipsLight } from "./fig-3-use-chips-to-enhance-the-experience.png";
+export { default as Fig3ActionChipsDark } from "./fig-3-use-chips-to-enhance-the-experience-dark.png";
+export { default as Fig4ActionChipsLight } from "./fig-4-do-not-use-chips-to-progress-through-a-process-or-perform-significant-actions.png";
+export { default as Fig4ActionChipsDark } from "./fig-4-do-not-use-chips-to-progress-through-a-process-or-perform-significant-actions-dark.png";
