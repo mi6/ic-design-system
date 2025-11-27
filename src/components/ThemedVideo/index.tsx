7fc927b5c2b7eaa231be31c3fa3a3ee0bdadd505
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2025-11-27 11:07:16 +0000

    feat(root): add dark mode support to DoDontCaution component, add state prop to ThemedVideo
    Update DoDontCaution component to support dark mode. Add ability to set a "do", "don't" or
    "caution" state to ThemedVideoComponent
    
    . #1682

--- src/components/ThemedVideo/index.tsx
+++ src/components/ThemedVideo/index.tsx
@@ -1,8 +1,12 @@
 /* eslint jsx-a11y/media-has-caption: 0 */
 import React from "react";
+import clsx from "clsx";
 import { IcTypography } from "@ukic/react";
+import Icon from "@mdi/react";
+import { mdiCheck, mdiClose, mdiAlert } from "@mdi/js";
 import { useTheme } from "../../context/ThemeContext";
 import { passImage } from "../../utils/helpers";
+import "./index.css";
 
 interface ThemedVideoProps {
   width?: string;
@@ -13,7 +17,14 @@ interface ThemedVideoProps {
   src: Array<string> | string;
   type?: any;
   caption?: string;
+  state?: "good" | "bad" | "none" | "caution";
 }
+
+const STATE_VALUES = {
+  good: { icon: mdiCheck, classPrefix: "do" },
+  bad: { icon: mdiClose, classPrefix: "dont" },
+  caution: { icon: mdiAlert, classPrefix: "caution" },
+};
 
 const ThemedVideo: React.FC<ThemedVideoProps> = ({
   width = "100%",
@@ -24,11 +35,27 @@ const ThemedVideo: React.FC<ThemedVideoProps> = ({
   src = "",
   type = "video/mp4",
   caption = "",
+  state = "none",
 }) => {
   const vidSrc = passImage(src, useTheme().theme);
 
   return (
-    <>
+    <div
+      className={clsx(
+        state === "none" ? state : STATE_VALUES[state].classPrefix
+      )}
+    >
+      {state !== "none" && (
+        <div
+          slot="icon"
+          className={clsx(
+            "do-dont-caution-containers",
+            `${STATE_VALUES[state].classPrefix}-icon`
+          )}
+        >
+          <Icon path={STATE_VALUES[state].icon} aria-hidden="true" />
+        </div>
+      )}
       <video
         key={vidSrc}
         width={width}
@@ -47,7 +74,7 @@ const ThemedVideo: React.FC<ThemedVideoProps> = ({
           {caption}
         </IcTypography>
       )}
-    </>
+    </div>
   );
 };
 
